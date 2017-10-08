dd67b8729e54b2005e52ff782c8697fb2b766eb1
Esco Obong <esco@hbyte.com>
2017-10-08 10:37:07 -0400

    format price

--- src/components/TokenList/index.js
+++ src/components/TokenList/index.js
@@ -11,17 +11,34 @@ const baseURL = process.env.NODE_ENV === 'production' ?
   'https://erc-20.io' :
   'http://192.168.86.22:3000'
 
-const TokenItem = ({item}) => (
-  <View style={styles.listItem}>
+const formatPrice = (price) => {
+  let [whole, decimal] = String(price).split('.')
+  if (whole === '0') {
+    return decimal
+  }
+  if (!decimal || Number.isNaN(decimal) || decimal === 'NaN') {
+    decimal = '.00'
+  } else {
+    decimal = '.' + decimal.substr(0,2)
+    if (decimal.length < 3) {
+      decimal += '0'
+    }
+  }
+  return Number(whole + decimal).toLocaleString()
+}
+
+const TokenItem = ({item, index}) => (
+  <View style={[styles.listItem, index == 0 ? styles.noBorderTop : {}]}>
     <View>
-      <Image source={{ uri: baseURL + item.imageUrl}} style={{width: 25, height: 25}} />
+      <Image source={{ uri: baseURL + item.imageUrl }} style={{width: 30, height: 30}} />
     </View>
 
     <View style={styles.symbolContainer}>
       <Text style={styles.symbol}>{item.symbol}</Text>
+      <Text style={styles.balance}>{item.balance}</Text>
     </View>
     <View style={[styles.priceContainer, parseInt(item.change) > -1 ? styles.gain : {}]}>
-      <Text>${(item.balance * item.price).toPrecision(6)}</Text>
+      <Text style={styles.price}>${formatPrice(item.balance * item.price)}</Text>
     </View>
   </View>
 );
@@ -44,7 +61,7 @@ class TokenList extends Component {
           style={styles.container}
           renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
           sections={[
-            {data: dataTokens, title: 'Holdings', renderItem: ({item}) => <TokenItem item={item}/>},
+            {data: dataTokens, title: 'Holdings', renderItem: ({item, index}) => <TokenItem item={item} index={index}/>},
           ]}
         />
       </View>
@@ -58,30 +75,47 @@ const styles = StyleSheet.create({
     backgroundColor: '#000'
   },
   symbol: {
-    color: '#fff'
+    color: '#fff',
+    fontSize: 16
+  },
+  balance: {
+    color: '#333',
+    fontSize: 12
   },
   symbolContainer: {
     flex: .6,
   },
   priceContainer: {
     flex: .2,
-    height: 30,
+    height: 40,
     backgroundColor: 'red',
     borderRadius: 8,
     borderWidth: 2,
     borderColor: 'red',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 10,
+    paddingLeft: 15,
+    paddingRight: 15
   },
   price: {
     color: '#000',
+    textAlign: 'center',
+    width: 85
   },
   listItem: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    marginBottom: 10,
-    padding: 10
+    marginLeft: 15,
+    padding: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderColor: '#111',
+    borderTopWidth: 1
+  },
+  noBorderTop: {
+    borderTopWidth: 0
   },
   sectionHeader: {
     color: "#fff",
