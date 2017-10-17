4f29f83e3534a0270e3de6d8a768a7a0cb4cfac2
Chinedu Daniel <chinedudaniel7@gmail.com>
2017-10-17 23:18:46 +0100

    Update tokens meta page to better follow mockup design

--- src/components/TokenDetails/index.js
+++ src/components/TokenDetails/index.js
@@ -13,14 +13,24 @@ const styles = StyleSheet.create({
   },
   container: {
     display: 'flex',
+    borderColor: '#fff',
     justifyContent: 'space-between',
     backgroundColor: '#000',
+    marginBottom: 10,
+    flexDirection: 'row',
+    paddingLeft: 20
   },
   containerChild: {
-    marginBottom: 10
+    marginBottom: 10,
+    flexGrow: .5,
+    flexBasis: 1
   },
-  tokenColor: {
-    color: '#fff'
+  tokenHeading: {
+    color: '#666'
+  },
+  tokenValue: {
+    color: '#fff',
+    fontSize: 30
   },
   header: {
     backgroundColor: '#000'
@@ -31,6 +41,7 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 22,
     paddingBottom: 10,
+    paddingLeft: 20,
     marginBottom: 10,
     marginTop: 10
   }
@@ -38,12 +49,12 @@ const styles = StyleSheet.create({
 
 class TokenDetails extends Component {
   componentWillMount = async () => {
-    const { getTokenDetails } = this.props
-    await getTokenDetails(this.props.symbol)
+    const { getTokenDetails, symbol } = this.props
+    await getTokenDetails(symbol)
   }
 
   render() {
-    const { price, quantity, marketCap, volume24Hr, totalValue } = this.props.tokenDetails
+    const { price, quantity, marketCap, volume24Hr, totalValue } = this.props.tokenDetails;
 
     return (
       <ScrollView style={styles.scrollContainer} containerStyleContent={styles.container}>
@@ -54,25 +65,25 @@ class TokenDetails extends Component {
 
         <View style={styles.container}>
           <View style={styles.containerChild}>
-            <Text style={styles.tokenColor}>Price</Text>
-            <Text style={styles.tokenColor}>{price}</Text>
+            <Text style={styles.tokenHeading}>PRICE</Text>
+            <Text style={styles.tokenValue}>{price.toFixed(2)}</Text>
           </View>
 
           <View style={styles.containerChild}>
-            <Text style={styles.tokenColor}>Quantity</Text>
-            <Text style={styles.tokenColor}>{quantity}</Text>
+            <Text style={styles.tokenHeading}>QUANTITY</Text>
+            <Text style={styles.tokenValue}>{quantity}</Text>
           </View>
         </View>
 
         <View style={styles.container}>
           <View style={styles.containerChild}>
-            <Text style={styles.tokenColor}>MarketCap</Text>
-            <Text style={styles.tokenColor}>{marketCap}</Text>
+            <Text style={styles.tokenHeading}>MARKET CAP</Text>
+            <Text style={styles.tokenValue}>{marketCap}</Text>
           </View>
 
           <View style={styles.containerChild}>
-            <Text style={styles.tokenColor}>24 hr Volume</Text>
-            <Text style={styles.tokenColor}>{volume24Hr}</Text>
+            <Text style={styles.tokenHeading}>24 HR VOLUME</Text>
+            <Text style={styles.tokenValue}>{volume24Hr}</Text>
           </View>
         </View>
       </ScrollView>
