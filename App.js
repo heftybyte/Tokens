5b540c6c96f65d13b2d9e412f28f9e31a2c8e0fa
Daniel Onodje <danielonodje@gmail.com>
2017-10-25 12:05:34 +0100

    add sentry

--- App.js
+++ App.js
@@ -6,6 +6,12 @@ import store from './src/store/index';
 
 import AppWithNavigationState from './src/navigators/AppNavigator';
 
+import Sentry from 'sentry-expo';
+
+Sentry.enableInExpoDevelopment = true;
+
+Sentry.config('https://bf77d4d2ce4843ae909cca0b6d7675bb@sentry.io/235327').install();
+
 class Tokens extends React.Component {
 
   render() {
