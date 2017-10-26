9eca0b8be9d3ab75593470a14b24013292c98bdc
Chinedu Daniel <chinedudaniel7@gmail.com>
2017-10-26 20:11:23 +0100

    Integrate google analytics into the application

--- App.js
+++ App.js
@@ -2,17 +2,23 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
+
+import { Analytics, PageHit } from 'expo-analytics';
+
 import store from './src/store/index';
 
 import AppWithNavigationState from './src/navigators/AppNavigator';
 
 import Sentry from 'sentry-expo';
 
-Sentry.enableInExpoDevelopment = true;
-
 const publicDNS = process.env.SENTRY_PUBLIC_DNS || 'https://bf77d4d2ce4843ae909cca0b6d7675bb@sentry.io/235327';
+const analytics = new Analytics('UA-108735063-1');
 
+analytics.hit(new PageHit('Home'));
+
+Sentry.enableInExpoDevelopment = true;
 Sentry.config(publicDNS).install();
+
 
 class Tokens extends React.Component {
 
