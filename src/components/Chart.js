9813fb64c36ed4df91b80fac1c278ac7a1e7026d
Ifeoluwa King <ifeoluwaking24@gmail.com>
2017-11-28 16:33:54 +0100

    line chart added

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import { withDrawer } from '../helpers/drawer';
 import { connect } from 'react-redux';
-import { VictoryChart, VictoryLine } from "victory-native";
+import { VictoryChart, VictoryLine, VictoryTheme } from "victory-native";
 
 class Chart extends Component {
 
     render() {
         return (
-            <VictoryChart/>
+            <VictoryChart>
+            <VictoryLine
+                style={{
+                data: { stroke: "#c43a31" },
+                parent: { border: "1px solid #ccc"}
+                }}
+                data={[
+                { x: 1, y: 2 },
+                { x: 2, y: 3 },
+                { x: 3, y: 5 },
+                { x: 4, y: 4 },
+                { x: 5, y: 7 }
+                ]}
+            />
+            </VictoryChart>
         )
     }
 }
