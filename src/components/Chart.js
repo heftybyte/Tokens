4ad32e3b58f359476f5e8e634a8c7e6126429d79
Ifeoluwa King <ifeoluwaking24@gmail.com>
2017-11-29 21:18:30 +0100

    changes to price chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,27 +1,43 @@
 import React, { Component } from "react";
+import {
+  StyleSheet,
+  View, Text
+} from 'react-native';
+import { Button } from 'native-base';
 import { withDrawer } from '../helpers/drawer';
 import { connect } from 'react-redux';
-import { VictoryChart, VictoryLine, VictoryTheme } from "victory-native";
+import { VictoryChart, VictoryLine, VictoryAxis, VictoryTheme } from "victory-native";
+
+
+
+const day = [{ x: '1D', y: 2 },{ x: '1W', y: 3 },{ x: '1M', y: 5 },{ x: '4M', y: 4 },{ x: '5M', y: 7 }]
+const periods = ['1D', '1W', '1M', '2M', '1Y', 'ALL']
 
 class Chart extends Component {
 
     render() {
+        let chartBtns = periods.map((x, i)=>(
+            <Button key={i} transparent style={{marginLeft: 5, marginRight: 5, paddingLeft: 10, paddingRight: 10}}><Text style={{color: '#6b2fe2'}}>{x}</Text></Button>
+        ))
         return (
+            <View>
             <VictoryChart>
+                
+                <VictoryAxis
+                dependentAxis
+                />
             <VictoryLine
                 style={{
-                data: { stroke: "#c43a31" },
+                data: { stroke: "#6b2fe2" },
                 parent: { border: "1px solid #ccc"}
                 }}
-                data={[
-                { x: 1, y: 2 },
-                { x: 2, y: 3 },
-                { x: 3, y: 5 },
-                { x: 4, y: 4 },
-                { x: 5, y: 7 }
-                ]}
+                data={day}
             />
             </VictoryChart>
+                <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+                    {chartBtns}
+                </View>
+            </View>
         )
     }
 }
