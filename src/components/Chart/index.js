1dc450af605104dd17cd7d2f83147b5d09b91701
Ifeoluwa King <ifeoluwaking24@gmail.com>
2017-12-01 22:27:01 +0100

    removed axis

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import { Button } from 'native-base';
-import { VictoryContainer, VictoryChart, VictoryLine, VictoryAxis, VictoryTheme } from "victory-native";
+import { VictoryContainer, VictoryGroup, VictoryCursorContainer, VictoryLine, VictoryAxis, VictoryTheme } from "victory-native";
 import { gainColor, lossColor } from '../../config'
 
 const styles = StyleSheet.create({
@@ -27,7 +27,7 @@ class Chart extends Component {
 
     render() {
         const { data, totalChangePct } = this.props
-        const gain = (totalChangePct||0) > 0
+        const gain = totalChangePct >= 0
         let chartBtns = periods.map((x, i)=>(
             <Button onPress={()=>this.setState({selected: i})} key={i} transparent style={styles.chrtBtn}>
                 <Text style={{color: this.state.selected===i?'#fff':'#6b2fe2'}}>{x}</Text>
@@ -36,19 +36,20 @@ class Chart extends Component {
 
         return (
             <View>
-            <VictoryContainer height={180} width={100} responsive={false}>
-            <VictoryChart height={240} padding={{ top: 5, bottom: 60, left: 40, right: 40 }}>
-                <VictoryAxis
-                dependentAxis crossAxis
-                style={{ axis: {stroke: "#ccc"} }}
+            <VictoryGroup height={170} padding={{ top: 5, bottom: 0, left: 20, right: 20 }}
+            containerComponent={
+                <VictoryCursorContainer
+                cursorDimension="y"
+                cursorLabel={(d) => `guygiughiuhiohiuguyftyf`}
+                style={{ labels: { fill: "#fff", stroke: "#fff", strokeWidth: 5 } }}
                 />
+            }>
             <VictoryLine
                 style={{ data: { stroke: gain ? gainColor : lossColor } }}
                 data={data.slice(0, data.length * ((this.state.selected + 1) * (100/periods.length))/100)}
             />
-            </VictoryChart>
+            </VictoryGroup>
                 
-            </VictoryContainer>
             <View style={styles.btnWrapper}>
                     {chartBtns}
                 </View>
