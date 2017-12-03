27879a06499962e63ad5f2efc6594d00c3bc1086
Ifeoluwa King <ifeoluwaking24@gmail.com>
2017-12-03 10:27:51 +0100

    added chart config  constructor

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { StyleSheet } from 'react-native' 
 import { gainColor, lossColor } from '../../config'
 import ChartView from 'react-native-highcharts'
-import {conf, options} from './conf'
+import {ChartConfig} from './conf'
 
 const styles = StyleSheet.create({
     chart: {
@@ -15,9 +15,9 @@ class Chart extends Component {
 
     render() {
         const { data, totalChangePct } = this.props
-        console.log(data)
+        let config = new ChartConfig(totalChangePct >= 0?gainColor:lossColor)
         return (
-            <ChartView style={{height:300}} config={conf} options={options} stock={true} style={styles.chart}></ChartView>                
+            <ChartView style={{height:300}} config={config.conf} options={config.options} stock={true} style={styles.chart}></ChartView>                
         )
     }
 }
