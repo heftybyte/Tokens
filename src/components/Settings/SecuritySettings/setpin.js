a2aa76167ae59adbc8499b86ba046731e6577758
Esco Obong <esco@hbyte.com>
2018-04-05 11:57:00 -0400

    styling and grammar

--- src/components/Settings/SecuritySettings/setpin.js
+++ src/components/Settings/SecuritySettings/setpin.js
@@ -3,9 +3,12 @@ import { connect } from 'react-redux';
 import { withDrawer } from '../../../helpers/drawer';
 import { NavigationActions } from 'react-navigation';
 import { StyleSheet, View, AsyncStorage, Alert, TouchableHighlight, Platform, TextInput } from 'react-native';
-import { Container, Label, Input,Form, Content, ListItem, Text, Radio, Footer, Button, Item} from 'native-base';
+import { Container, Label, Input,Form, Content, ListItem, Text, Radio, Footer, Button, Item, StyleProvider } from 'native-base';
 import { getPin, storePin } from '../../../helpers/security'
 import { enablePin } from '../../../actions/security';
+import { brandColor } from '../../../config'
+import getTheme from '../../../../native-base-theme/components';
+import platform from '../../../../native-base-theme/variables/platform';
 
 const styles = StyleSheet.create({
     white: {
@@ -22,6 +25,11 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 20
     },
+    title: {
+        color: brandColor,
+        fontSize: 18,
+        marginBottom: 20
+    }
 })
 
 class SetPin extends Component {
@@ -31,6 +39,7 @@ class SetPin extends Component {
         "oldPin": null,
         "confirmPin": null,
         'pinExists': false,
+        pinLength: 6
     }
 
     async componentDidMount() {
@@ -46,35 +55,25 @@ class SetPin extends Component {
     }
 
     handleChange (event){
-        Alert.alert('change')
-        console.log('event')
-        console.log(event)
         this.setState({
             [event.name]: event.value
         })
     }
 
     changePin = async function() {
-        Alert.alert('in set pin')
-
         const storedPin = await getPin()
-        if(storedPin == this.state.oldPin){
+        if(storedPin === this.state.oldPin){
             await this.setPin()
             return
         }
-        
-        Alert.alert('wrong previous pin')
+        Alert.alert('Invalid Previous Pin')
     }
 
     setPin = async function() {
-        Alert.alert('in set pin')
-        console.log('newPin')
-        console.log(this.state.newPin)
-        console.log(this.state.confirmPin)
-
-        if(this.state.newPin.length != 4 || this.state.confirmPin.length!=4){
+        const { newPin, confirmPin, pinLength } = this.state
+        if(newPin.length != pinLength || confirmPin.length != pinLength){
             // error
-            Alert.alert('please check pin')
+            Alert.alert('Invalid Pin')
             return
         }
 
@@ -85,64 +84,71 @@ class SetPin extends Component {
         }
 
         const result = await storePin(this.state.newPin)
-        console.log(result)
 
-        Alert.alert('pin set successfully')
+        Alert.alert('Pin Set Successfully')
         this.props.goToSecuritySettingsPage()
     }
 
     render () {
+        const { pinLength } = this.state
         return(
-            <Container>
-                <Content>
-                    <Text style={styles.white}>Enter your 4 digit pin</Text>
-                    <Form>
-                        {
-                            (this.state.pinExists) ?
-                                <View>
-                                    <Label>Old Pin</Label>
-                                    <TextInput
-                                        contextMenuHidden
-                                        secureTextEntry
-                                        style={styles.input}
-                                        onChangeText={(text) => { this.setState({oldPin: text}); }}
-                                        placeholderTextColor={'#333'}
-                                        autoCapitalize={'characters'}
-                                    />
-                                </View>
-                                : <View />
-                        }
+            <StyleProvider style={getTheme(platform)}>
+                <Container>
+                    <Content style={{paddingHorizontal:20}}>
+                        <Text style={styles.title}>Enter a {pinLength} Digit Pin Code</Text>
+                        <Form>
+                            {
+                                (this.state.pinExists) ?
+                                    <View>
+                                        <Label style={styles.white}>Old Pin</Label>
+                                        <TextInput
+                                            contextMenuHidden
+                                            secureTextEntry
+                                            style={styles.input}
+                                            onChangeText={(text) => { this.setState({oldPin: text}); }}
+                                            placeholderTextColor={'#333'}
+                                            placeholder={'Enter Old Pin'}
+                                            autoCapitalize={'characters'}
+                                        />
+                                    </View>
+                                    : <View />
+                            }
 
-                            <Label>New Pin</Label>
-                            <TextInput
-                                name="newPin"
-                                style={styles.input}
-                                value={this.state.newPin}
-                                onChangeText={(text)=>{ this.setState({newPin: text}); }}
-                                contextMenuHidden
-                                secureTextEntry
-                            />
-                            <Label>Confirm Pin</Label>
-                            <TextInput
-                                contextMenuHidden
-                                secureTextEntry
-                                style={styles.input}
-                                onChangeText={(text)=>{  this.setState({confirmPin: text}); }}
-                                value={this.state.confirmPin}
-                            />
-                    </Form>
-                    {
-                        this.state.pinExists ?
-                            <Button full success onPress={this.changePin.bind(this)}>
-                                <Text>Change Pin</Text>
-                            </Button>
-                            :
-                            <Button full success onPress={this.setPin.bind(this)}>
-                                <Text>Set Pin</Text>
-                            </Button>
-                    }
-                </Content>
-            </Container>
+                                <Label style={styles.white}>New Pin</Label>
+                                <TextInput
+                                    name="newPin"
+                                    style={styles.input}
+                                    value={this.state.newPin}
+                                    onChangeText={(text)=>{ this.setState({newPin: text}); }}
+                                    contextMenuHidden
+                                    placeholderTextColor={'#333'}
+                                    placeholder={'Enter New Pin'}
+                                    secureTextEntry
+                                />
+                                <Label style={styles.white}>Confirm Pin</Label>
+                                <TextInput
+                                    contextMenuHidden
+                                    secureTextEntry
+                                    placeholder={'Confirm Pin'}
+                                    placeholderTextColor={'#333'}
+                                    style={styles.input}
+                                    onChangeText={(text)=>{  this.setState({confirmPin: text}); }}
+                                    value={this.state.confirmPin}
+                                />
+                        </Form>
+                        {
+                            this.state.pinExists ?
+                                <Button full primary onPress={this.changePin.bind(this)}>
+                                    <Text style={{color: '#000'}}>Change Pin</Text>
+                                </Button>
+                                :
+                                <Button full primary onPress={this.setPin.bind(this)}>
+                                    <Text style={{color: '#000'}}>Set Pin</Text>
+                                </Button>
+                        }
+                    </Content>
+                </Container>
+            </StyleProvider>
         )
     }
 }
