a2dd20019f32fe67e1977178f859a01ca9e374ad
Esco Obong <esco@hbyte.com>
2018-04-09 12:46:09 -0400

    remove mobx, redux registration, google login

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,3 +1,143 @@
-export { default } from './register'
-export { Normal as NormalRegistration } from './normal'
-export { Guest as GuestRegistration } from './guest'
+import React, { Component } from 'react';
+import { Alert, Image, TouchableHighlight, TouchableWithoutFeedback, WebView } from 'react-native';
+import QRButton from '../Common/QRButton'
+import { NavigationActions } from 'react-navigation';
+import { MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
+import { baseAccent, baseColor, brandColor, lossColor } from '../../config'
+import { View, Container, Header, Content, ListItem, Input,Text,
+    Radio, Footer, Button, CheckBox, Body, Right, List, Label, Item, Form, StyleProvider } from 'native-base';
+import { addExchangeAccount } from '../../helpers/api';
+import { withDrawer } from '../../helpers/drawer';
+import { SecureStore} from 'expo'
+import { constants } from '../../constants';
+import { connect } from 'react-redux';
+import getTheme from '../../../native-base-theme/components';
+import _platform from '../../../native-base-theme/variables/platform';
+import styles from './styles'
+
+const customStyles = {
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'center'
+    },
+    header: {
+        paddingHorizontal: 20,
+        paddingVertical: 10
+    },
+    heading: {
+        fontSize: 16,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        borderTopWidth: 0,
+        borderWidth: 0
+    },
+    imageContainer: {
+        flex: .6,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        marginBottom: 50
+    },
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        marginVertical: 10
+    },
+    image: {
+        width: 192,
+        height: 169.6
+    },
+    buttonContainer: {
+        flex: .4,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        flex: .5,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginBottom: 10
+    },
+    buttonText: {
+        color: '#000',
+        flex: .5
+    },
+    googleButton: {
+        backgroundColor: '#fff'
+    }
+}
+
+class Register extends Component {
+
+    static hideHeader = true
+
+    state = {
+        username: '',
+        password: '',
+        email: ''
+    }
+
+    submit = () => {
+        const { username, email, password } = this.state
+        console.log({ username, email, password })
+    }
+
+    render() {
+        const { navigate } = this.props
+        return (
+            <StyleProvider style={getTheme(_platform)}>
+                <Container>
+                    <Content style={customStyles.container}>
+                        <View style={customStyles.imageContainer}>
+                            <Image source={require('../../../assets/Tokens_Icon.png')} style={customStyles.image} />
+                            <Text style={customStyles.title}>Tokens Express</Text>
+                        </View>
+                        <View style={customStyles.buttonContainer}>
+                            <View style={customStyles.button}>
+                                <Button
+                                    style={[{flex: 1}, customStyles.googleButton]}
+                                    primary
+                                    title={"Continue With Google"}
+                                    block
+                                    onPress={() => navigate('SignUp', { withGoogle: true })}>
+                                    <FontAwesome style={{flex: .1, padding: 10, paddingRight: 0}} color={baseColor} name={'google'} size={30} />
+                                    <Text style={customStyles.buttonText}>CONTINUE WITH GOOGLE</Text>
+                                </Button>
+                            </View>
+                            <View style={customStyles.button}>
+                                <Button
+                                    style={{flex: 1}}
+                                    primary
+                                    title={"Sign Up With Email"}
+                                    block
+                                    onPress={() => navigate('SignUp')}>
+                                    <MaterialCommunityIcons style={{flex: .1 }} color={baseColor} name={'email'} size={30} />
+                                    <Text style={customStyles.buttonText}>SIGN UP WITH EMAIL</Text>
+                                </Button>
+                            </View>
+                            <View style={{flex: 1, flexDirection: 'row', alignSelf: 'center'}}>
+                                <Text style={{color: '#fff'}}>Already have an account?</Text>
+                                <TouchableWithoutFeedback onPress={()=>navigate('Login')}>
+                                    <Text style={{color: brandColor}}> Sign in.</Text>
+                                </TouchableWithoutFeedback>
+                            </View>
+                        </View>
+                    </Content>
+                </Container>
+            </StyleProvider>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    ...state.ui
+})
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        navigate: (routeName, params={}) => dispatch(NavigationActions.navigate({ routeName, params }))
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(withDrawer(Register));
--- src/config.js
+++ src/config.js
@@ -11,5 +11,7 @@ export const gainColor = '#27A459'
 export const lossColor = '#BE2B2F'
 export const brandColor = '#5EECF8'
 export const invitesEnabled = false
+export const GOOGLE_CLIENT_ID_IOS = '947782058234-bi34g7jp0k6gf50i51clisna8j7esaer.apps.googleusercontent.com'
+export const GOOGLE_CLIENT_ID_ANDROID = '947782058234-8q3galj7afvdate88ceial9mnpg9t0dl.apps.googleusercontent.com'
 
 console.log({ baseURL })
